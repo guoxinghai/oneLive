947fba745f9e146f33ac3557ffaee7684fe44005
GuoXingHai <42136088+guoxinghai@users.noreply.github.com>
2019-08-28 10:50:31 +0800

    删除bmob数据存储id

--- app/src/main/java/com/onlive/util/MyApplication.java
+++ app/src/main/java/com/onlive/util/MyApplication.java
@@ -8,7 +8,7 @@ import cn.bmob.v3.Bmob;
 public class MyApplication extends Application {
     private static Context context;
     private boolean status;//存储用户的登录状态
-    private String ApplicationID = "541430194a826f4196a7a6b460e72573";//bmob数据存储id
+    private String ApplicationID = "";//bmob数据存储id
     @Override
     public void onCreate() {
         super.onCreate();
